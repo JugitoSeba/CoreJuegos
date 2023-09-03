cbc21511cb2ce4c26487846c6c5338d09efeaa90
JugitoSeba <sebacenturion100@gmail.com>
2023-09-03 16:49:16 -0300

    Configuración De Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//FIREBASE
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+   firebaseConfig: {
+        apiKey: "AIzaSyDEjMxe4_2lr_5HR5v2PLgZC9CmowhcQBk",
+        authDomain: "corejuegos-61b36.firebaseapp.com",
+        projectId: "corejuegos-61b36",
+        storageBucket: "corejuegos-61b36.appspot.com",
+        messagingSenderId: "409823649742",
+        appId: "1:409823649742:web:a6f9443bd2648d5687959f",
+        measurementId: "G-D39RNB9NFB"
+    }
+}
