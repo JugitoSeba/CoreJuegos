c81ea59965af4b33b8aee80940a32581d4c11041
JugitoSeba <sebacenturion100@gmail.com>
2023-09-21 16:15:19 -0300

    Registro y su ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
+import { RegistroComponent } from "./modules/registro/pages/registro/registro.component"
 
 
 const routes: Routes = [
@@ -9,6 +10,12 @@ const routes: Routes = [
   },
   {
     path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  },
+  {
+    path:"registro",component:RegistroComponent
+  },
+  {
+    path:"registro",loadChildren:()=>import('./modules/registro/registro.module').then(m=>m.RegistroModule)
   }
 ];
 
--- src/app/modules/registro/registro-routing.module.ts
+++ src/app/modules/registro/registro-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from "./pages/registro/registro.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:RegistroComponent
+  },
+  {
+    path:"registro",component:RegistroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
