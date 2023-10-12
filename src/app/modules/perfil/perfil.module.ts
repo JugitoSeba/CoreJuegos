0a93dc6823e7302349c0ee53ef5b8370f49c0445
JugitoSeba <sebacenturion100@gmail.com>
2023-10-12 11:51:03 -0300

    Pagina Pefil

--- src/app/modules/perfil/perfil.module.ts
+++ src/app/modules/perfil/perfil.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PerfilRoutingModule } from './perfil-routing.module';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
   ],
   imports: [
     CommonModule,
-    PerfilRoutingModule
+    PerfilRoutingModule,
+    SharedModule
   ]
 })
 export class PerfilModule { }
