4594691dad67589dcbc41008cab08e7dee1cc43f
JugitoSeba <sebacenturion100gmail.com>
2023-10-26 08:24:35 -0300

    Creacion modulo plataformas

---
+++ src/app/modules/plataformas/plataformas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlataformasRoutingModule } from './plataformas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlataformasRoutingModule
+  ]
+})
+export class PlataformasModule { }
