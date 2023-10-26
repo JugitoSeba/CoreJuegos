f0f7fd9ff947de2769bd408083c8cc09a5095d57
JugitoSeba <sebacenturion100gmail.com>
2023-10-26 08:37:00 -0300

    Pagina de plataformas, su routing

--- src/app/modules/plataformas/plataformas-routing.module.ts
+++ src/app/modules/plataformas/plataformas-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlataformasComponent } from './pages/plataformas/plataformas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"plataformas",component:PlataformasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
