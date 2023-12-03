12795133b6784e41176db984623973b570bc0675
JugitoSeba <sebacenturion100@gmail.com>
2023-12-03 20:41:35 -0300

    css,ts e implementacion de card-xbox

--- src/app/modules/search/components/card-xbox/card-xbox.component.ts
+++ src/app/modules/search/components/card-xbox/card-xbox.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
 @Component({
   selector: 'app-card-xbox',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-xbox.component.css']
 })
 export class CardXboxComponent {
+  coleccionProductos: Producto[] = []; // todos los productos
 
+  xbox: Producto[] = []; // productos categoría = xbox
+
+  productoSeleccionado!: Producto;
+
+  modalVisible: boolean = false;
+
+  constructor(
+    public servicioCrud: CrudService
+  ){}
+
+  ngOnInit(): void{
+    this.servicioCrud.obtenerProducto().subscribe(producto =>{
+      this.coleccionProductos = producto;
+
+      this.mostrarXbox();
+    })
+  }
+
+  mostrarVer(info: Producto){
+    // al seleccionar "ver más" el modal visible pasa a "true"
+    this.modalVisible = true;
+    // muestra la información del producto que se seleccione
+    this.productoSeleccionado = info;
+  }
+
+  mostrarXbox(){
+    // forEach itera la colección/ arreglo
+    this.coleccionProductos.forEach(producto =>{
+      /* si la categoría del producto es igual a "collares"
+        se va a enviar a la colección "collares"*/
+      if(producto.categoria === "XBOX"){
+        this.xbox.push(producto);
+      }
+    })
+  }
 }
