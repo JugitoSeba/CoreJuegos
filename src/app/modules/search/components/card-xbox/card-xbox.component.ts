2d204f3a42c143b3f06a5f4bcbe5dcacb44e6c15
JugitoSeba <sebacenturion100@gmail.com>
2023-12-03 20:38:20 -0300

    cracion de componente card-xbox

---
+++ src/app/modules/search/components/card-xbox/card-xbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-xbox',
+  templateUrl: './card-xbox.component.html',
+  styleUrls: ['./card-xbox.component.css']
+})
+export class CardXboxComponent {
+
+}
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -6,13 +6,15 @@ import { SearchComponent } from './pages/search/search.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CardPs4Component } from './components/card-ps4/card-ps4.component';
 import { CardPs3Component } from './components/card-ps3/card-ps3.component';
+import { CardXboxComponent } from './components/card-xbox/card-xbox.component';
 
 
 @NgModule({
   declarations: [
     SearchComponent,
     CardPs4Component,
-    CardPs3Component
+    CardPs3Component,
+    CardXboxComponent
   ],
   imports: [
     CommonModule,
